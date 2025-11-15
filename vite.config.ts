1e9a55c876a762793ed48fcec283c2ed53d6e32b
Santiago Souto <86037575+DevSolto@users.noreply.github.com>
2025-11-15 00:10:38 -0300

    Reconfigure Tailwind for v3

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import tailwindcss from "@tailwindcss/vite";
 
 
 const apiProxyRoutes = [
@@ -60,9 +59,7 @@ export default defineConfig(({ mode }) => {
     coerceBoolean(env.ENABLE_API_PROXY, true);
 
   return {
-    plugins: [react(),
-    tailwindcss(),
-    ],
+    plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
